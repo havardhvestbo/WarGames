52d6c877407e5ba7a8bccd473d6c3dc39befcb4e
Håvard Hetland Vestbø <havard@vestbo.net>
2022-02-21 09:20:48 +0100

    Commiter Unit klasse v. 1.0

---
+++ src/main/java/no/ntnu/iir/wargames/Unit.java
@@ -0,0 +1,90 @@
+package no.ntnu.iir.wargames;
+
+/**
+ * A class representing shared characteristics of Units
+ *
+ * @author Håvard H. Vestbø
+ * @version snapshot 1.0
+ */
+
+public abstract class Unit {
+
+    private String name;
+    private int health;
+    private int attack;
+    private int armor;
+
+    /**
+     * Creates an instance of a Unit.
+     *
+     * @param name
+     * @param health
+     * @param attack
+     * @param armor
+     */
+    public Unit(String name, int health, int attack, int armor) {
+        this.name = name;
+        this.health = health;
+        //Health cannot be lower than 0
+        this.attack = attack;
+        this.armor = armor;
+    }
+
+    /**
+     * Changes the healt of the oppenent after the units atttck.
+     * @param opponent
+     */
+    protected void attack(Unit opponent) {
+        //health = health - (attack + attackBonus) + (armor + resistBonus)
+    }
+
+    /**
+     * Returns the name of the unit.
+     *
+     * @return name og unit.
+     */
+    protected String getName() {
+        return name;
+    }
+
+    /**
+     * Returns health of the unit.
+     *
+     * @return health.
+     */
+    protected int getHealth() {
+        return health;
+    }
+
+    /**
+     * Returns the damage a unit deals.
+     *
+     * @return attack.
+     */
+    protected int getAttack() {
+        return attack;
+    }
+
+    /**
+     * Returns the armor level left on the unit
+     *
+     * @return armor.
+     */
+    protected int getArmor() {
+        return armor;
+    }
+
+    /**
+     * Changes the health of the unit.
+     */
+    protected void setHealth(int newHealth) {
+        health = newHealth;
+    }
+
+
+
+
+
+
+
+}
