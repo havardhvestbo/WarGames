be54a5089200ea07d41a7c330d03ec6f867e636c
Håvard Hetland Vestbø <haavarhv@stud.ntnu.no>
2022-05-24 13:16:36 +0200

    Exam v1.4

--- src/main/java/no/ntnu/iir/wargames/service/FileHandler.java
+++ src/main/java/no/ntnu/iir/wargames/service/FileHandler.java
@@ -18,6 +18,13 @@ import no.ntnu.iir.wargames.data.Unit;
 
 public class FileHandler {
 
+  /**
+   * A file saver to save current csv files in the game.
+   *
+   * @param army army to save.
+   * @param fileName name of file.
+   * @throws FileNotFoundException
+   */
   public void saveFile(Army army, String fileName) throws FileNotFoundException {
     File file = new File(fileName);
     PrintWriter printWriter = new PrintWriter(file);
@@ -28,6 +35,13 @@ public class FileHandler {
       printWriter.close();
     }
   }
+
+  /**
+   * A file loader to read existing csv files (saves) and load into the game.
+   *
+   * @param fileName name of file.
+   * @return army.
+   */
   public Army loadFile(String fileName) {
     Path stringToPath = Paths.get(fileName);
     Army army = new Army("name");
@@ -69,53 +83,4 @@ public class FileHandler {
     }
     return army;
   }
-
 }
-//  public static void main(String[] args) throws URISyntaxException, IOException {
-//    fileReader("Human-Army");
-//  }
-//
-//  /**
-//   * A file reader to read existing csv files (saves) and load into the game.
-//   *
-//   * @param armyName file/army name.
-//   * @throws IOException
-//   */
-//  public static void fileReader(String armyName) throws IOException {
-//    try (BufferedReader reader = new BufferedReader(new FileReader( armyName + ".csv"))) {
-//      String line;
-//      while ((line= reader.readLine()) != null) {
-//        String[] unitAsSplitString = line.split(",");
-//        if (unitAsSplitString.length > 1){
-//          System.out.println(unitAsSplitString[1]);
-//        } else {
-//          System.out.println("The army's name is : " + unitAsSplitString[0]);
-//        }
-//      }
-//
-//    } catch (IOException e) {
-//      throw e;
-//    }
-//  }
-//
-//  /**
-//   * A file writer to save current csv files in the game.
-//   *
-//   * @param army army to save
-//   * @throws IOException
-//   */
-//  public static void fileWriter(Army army) throws IOException {
-//    List<Unit> units = army.getAllUnits();
-//    try (BufferedWriter writer = new BufferedWriter(new FileWriter(army.getArmyName()+ ".csv"))){
-//      writer.write(army.getArmyName());
-//      writer.newLine();
-//      for (Unit unit : units) {
-//        String lineToBeWritten = unit.getClass().getSimpleName()+ "," + unit.getName() + "," + unit.getHealth();
-//        writer.write(lineToBeWritten);
-//        writer.newLine();
-//      }
-//    } catch (IOException e) {
-//      throw e;
-//    }
-//  }
-//}
